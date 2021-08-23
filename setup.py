bb441bb64c444f6384cd2d17c1a2172ee7dbe9f9
skybro2004 <67826154+skybro2004@users.noreply.github.com>
2021-08-23 10:20:34 +0900

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,34 @@
+def setup():
+    while 1:
+        token = input("디스코드 봇의 토큰을 입력해주세요 : ")
+        
+        if token=="":
+            print("디스코드 봇의 토큰을 정확히 입력해주세요!")
+        else:
+            break
+
+    with open("token.txt", "w") as tokenFile:
+        tokenFile.write(token)
+
+    while 1:
+        key = input("나이스 api 키를 입력해주세요 : ")
+        
+        if key=="":
+            print("api 키를 정확히 입력해주세요!")
+        else:
+            break
+
+    with open("modules/key.txt", "w") as keyFile:
+        keyFile.write(key)
+
+    device = input("봇을 호스팅할 기기의 이름을 입력해주세요(선택) : ")
+
+    if not(device==""):
+        with open("device.txt", "w") as deviceFile:
+            deviceFile.write(device)
+
+    return token
+
+
+if __name__=="__main__":
+    setup()
